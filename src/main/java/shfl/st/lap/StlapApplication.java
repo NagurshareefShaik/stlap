8b199108785dc0a1aa6a7dd337f63e1d073fcb68
Nagurshareef Shaik <nagurshareef123@gmail.com>
2022-11-28 17:24:59 +0530

    intial commit

---
+++ src/main/java/shfl/st/lap/StlapApplication.java
@@ -0,0 +1,16 @@
+package shfl.st.lap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class StlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shfl/st/lap/StlapApplicationTests.java
@@ -0,0 +1,13 @@
+package shfl.st.lap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
