3aa4ccfa4e61cafa910d86b19ac2ad59fa7f5916
gopi <gopi.m@ivtlinfoview.co.jp>
2023-01-03 12:26:14 +0530

    refs accural/waiver commit

--- src/main/java/shfl/st/lap/feeaccrual/model/AdditionalFeesHistoryPkey.java
+++ src/main/java/shfl/st/lap/feeaccrual/model/AdditionalFeesHistoryPkey.java
@@ -1,9 +1,12 @@
 package shfl.st.lap.feeaccrual.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class AdditionalFeesHistoryPkey implements Serializable{
 	private String applicationNumber;
 	private String feeType;
 	private Integer referenceNo;
+	private String modifiedContent;
+	private LocalDateTime modifiedDateTime;
 }
