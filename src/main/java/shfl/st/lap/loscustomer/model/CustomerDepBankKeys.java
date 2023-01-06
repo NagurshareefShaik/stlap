d791cd8ca7069cf8d1885a45b1f288387b8686ba
gopi <gopi.m@ivtlinfoview.co.jp>
2023-01-06 14:30:59 +0530

    refs los customer column name changes commit

--- src/main/java/shfl/st/lap/loscustomer/model/CustomerDepBankKeys.java
+++ src/main/java/shfl/st/lap/loscustomer/model/CustomerDepBankKeys.java
@@ -1,7 +1,8 @@
 package shfl.st.lap.loscustomer.model;
+
 import java.io.Serializable;
 
-public class CustomerDepBankKeys implements Serializable{
-	private String bankAccountNumber;
-	private String applicationNumber;
+public class CustomerDepBankKeys implements Serializable {
+	private String bankAccountNum;
+	private String applicationNum;
 }
--- src/main/java/shfl/st/lap/loscustomer/model/LosCustomer.java
+++ src/main/java/shfl/st/lap/loscustomer/model/LosCustomer.java
@@ -13,88 +13,88 @@ import shfl.st.lap.auditlog.Auditable;
 @Data
 @Entity
 @Table(name = "ST_TB_LMS_TEMP_CUSTOMER")
-public class LosCustomer extends Auditable{
-	
+public class LosCustomer extends Auditable {
+
 	@Id
-	@Column(name = "application_number")
-	private String applicationNumber;
-	
-	@Column(name = "request_number")
-	private String requestNumber;
-	
+	@Column(name = "application_num")
+	private String applicationNum;
+
+	@Column(name = "request_num")
+	private String requestNum;
+
 	@Column(name = "branch")
 	private String branch;
-	
+
 	@Column(name = "customer_id")
 	private int customerId;
-	
+
 	@Column(name = "customer_name")
 	private String customerName;
-	
-	@Column(name="disb_no")
-	private int disbNo;
-	
+
+	@Column(name = "disb_num")
+	private int disbNum;
+
 	@Column(name = "co_applicant_name")
 	private String coApplicantName;
-	
+
 	@Column(name = "customer_type")
 	private String customerType;
-	
+
 	@Column(name = "rate_of_interest")
 	private float rateOfInterest;
-	
+
 	@Column(name = "application_date")
 	private Date applicationDate;
-	
-	@Column(name = "loan_amount")
-	private float loanAmount;
-	
-	@Column(name = "sanction_amount")
-	private float sanctionAmount;
-	
+
+	@Column(name = "loan_amt")
+	private float loanAmt;
+
+	@Column(name = "sanction_amt")
+	private float sanctionAmt;
+
 	@Column(name = "effective_date")
 	private Date effectiveDate;
-	
+
 	@Column(name = "los_status")
 	private String losStatus;
-	
+
 	@Column(name = "legal_app_date")
 	private Date legalApprovedDate;
-	
+
 	@Column(name = "tech_app_date")
 	private Date technicalApprovedDate;
-	
+
 	@Column(name = "cr_app_date")
 	private Date creditApprovedDate;
-	
+
 	@Column(name = "rcu_app_date")
 	private Date rcuApprovedDate;
-	
+
 	@Column(name = "legal_deviation_status")
 	private String legalDeviationStatus;
-	
+
 	@Column(name = "techinical_deviation_status")
 	private String techinicalDeviationStatus;
-	
+
 	@Column(name = "cr_deviation_status")
 	private String creditDeviationStatus;
-	
+
 	@Column(name = "memo_paid")
 	private float memoPaid;
-	
+
 	@Column(name = "memo_waived")
 	private float memoWaived;
-	
+
 	@Column(name = "memo_outsanding")
 	private float memoOutsanding;
-	
+
 	@Column(name = "memo_deduction")
 	private float memoDeduction;
-	
+
 	@Column(name = "mobile_umber")
 	private int mobileNumber;
-	
+
 	@Column(name = "email_id")
 	private String emailId;
-	
+
 }
