6b6e0f8e9d9ee04806dbbed3df0e230056768ea3
Nagurshareef Shaik <42448704+NagurshareefShaik@users.noreply.github.com>
2023-01-25 10:06:57 +0530

    Revert "amort calculization and pdf generation"

--- src/main/java/shfl/st/lap/repaymentschedule/controller/RepaymentController.java
+++
@@ -1,8 +0,0 @@
-package shfl.st.lap.repaymentschedule.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RepaymentController {
-
-}
--- src/main/java/shfl/st/lap/repaymentschedule/repo/RepaymentRepo.java
+++
@@ -1,8 +0,0 @@
-package shfl.st.lap.repaymentschedule.repo;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class RepaymentRepo {
-
-}
